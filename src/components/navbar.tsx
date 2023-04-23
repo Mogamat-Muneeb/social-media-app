f9f5614da075779654e93e3f2bcefa71ab7c51e6
Mogamat-Muneeb <davimuneeb785@gmail.com>
2023-04-23 12:06:05 +0200

    add nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,37 +11,52 @@ export const Navbar = () => {
   };
   return (
     <div
-      className={`flex items-center max-w-[1228px] w-full mx-auto py-2 ${
-        user ? " justify-between" : " justify-center "
-      }`}
+      className={` sticky top-0 right-0 left-0  ${
+        user ? "bg-[#212529] text-white " : "text-black"
+      } `}
     >
-      <div className="flex gap-4">
-        <Link to="/" className="font-medium text-[16px]">
-          Home
-        </Link>
-        {user ? null : (
-          <Link to="/login" className="font-medium text-[16px]">
-            Login
+      <div
+        className={`flex items-center max-w-[1228px] w-full mx-auto py-2 ${
+          user ? " justify-between" : " justify-center "
+        }`}
+      >
+        <div className="flex gap-4">
+          <Link to="/" className="font-medium text-[16px]">
+            Home
           </Link>
-        )}
-      </div>
-      <div className="flex items-center gap-4">
-        {user && (
-          <>
-            <img
-              src={user?.photoURL || ""}
-              alt={user?.displayName || ""}
-              className="w-10 h-10 rounded-full"
-            />
-            <div className="flex flex-col text-start">
-              <p className="font-medium text-[14px]"> {user?.displayName} </p>
-              <p className="font-normal text-[14px]"> {user?.email} </p>
-            </div>
-            <button onClick={signUserOut} className="font-medium text-[16px]">
-              Logout
-            </button>
-          </>
-        )}
+          {user ? null : (
+            <Link to="/login" className="font-medium text-[16px]">
+              Login
+            </Link>
+          )}
+        </div>
+        <div className="flex items-center gap-4">
+          {user && (
+            <>
+              <img
+                src={user?.photoURL || ""}
+                alt={user?.displayName || ""}
+                className="w-10 h-10 rounded-full"
+              />
+              <div className="flex flex-col text-start">
+                <p className="font-medium text-[14px]">
+                  {user?.displayName
+                    ?.split(" ")
+                    .map(
+                      (word) =>
+                        word.substring(0, 1).toUpperCase() + word.substring(1)
+                    )
+                    .join(" ")}
+                </p>
+
+                <p className="font-normal text-[14px]"> {user?.email} </p>
+              </div>
+              <button onClick={signUserOut} className="font-medium text-[16px]">
+                Logout
+              </button>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
