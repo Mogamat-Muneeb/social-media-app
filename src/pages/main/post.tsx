b6cf55aace89498338a375b82e915722dffb520d
Mogamat-Muneeb <davimuneeb785@gmail.com>
2023-04-26 21:27:47 +0200

    wip

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -16,7 +16,8 @@ import { Post as IPost } from "./main";
 import { useNavigate, Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import { config } from "../../config/index";
-interface Props {
+import {UnLikedIcon ,LikedIcon} from '../../components/icon'
+ interface Props {
   post: IPost;
 }
 
@@ -219,7 +220,7 @@ export const Post = (props: Props) => {
                 <div className="flex items-center justify-center mt-32">
                   <svg
                     aria-hidden="true"
-                    className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 f"
+                    className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600"
                     viewBox="0 0 100 101"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
@@ -236,7 +237,7 @@ export const Post = (props: Props) => {
                 </div>
               </div>
             </>
-          )}{" "}
+          )}
           {/* show the loader if loading is true */}
           <img
             src={post.imageUrl}
@@ -255,37 +256,11 @@ export const Post = (props: Props) => {
                   >
                     {hasUserLiked ? (
                       <>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="#ff3040"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="#ff3040"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                          />
-                        </svg>
+                     <LikedIcon/>
                       </>
                     ) : (
                       <>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="black"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                          />
-                        </svg>
+                            <UnLikedIcon/>
                       </>
                     )}
                   </button>
@@ -325,37 +300,11 @@ export const Post = (props: Props) => {
                 >
                   {hasUserLiked ? (
                     <>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="#ff3040"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="#ff3040"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                        />
-                      </svg>
+                 <LikedIcon/>
                     </>
                   ) : (
                     <>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="black"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                        />
-                      </svg>
+              <UnLikedIcon/>
                     </>
                   )}
                 </button>
